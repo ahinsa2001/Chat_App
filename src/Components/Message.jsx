80bf08ebded68f0663b2cba652a7f84ef59e414b
Ahinsa Sathsarani <121052547+ahinsa2001@users.noreply.github.com>
2023-12-19 03:57:32 +0530

    Create ChatContext file and coding input and message sections

--- src/Components/Message.jsx
+++ src/Components/Message.jsx
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useContext, useEffect, useRef } from 'react'
+import { AuthContext } from '../context/AuthContext'
+import { ChatContext } from '../context/ChatContext';
 
-export const Message = () => {
+export const Message = ({message}) => {
+  
+  const {currentUser} = useContext(AuthContext);
+  const {data} = useContext(ChatContext);
+
+  const ref = useRef();
+
+  useEffect(() => {
+    ref.current?.scrollIntoView({ behavior:"smooth" });
+  }, [message]);
+
   return (
-    <div className='message owner'>
+    <div ref={ref}
+      className={'message ${message.senderId === currentUser.uid && "owner"}'}
+    >
        <div className="messageInfo">
-        <img src="https://c4.wallpaperflare.com/wallpaper/663/346/553/female-celebrities-genelia-d-souza-wallpaper-preview.jpg" 
+        <img 
+          src={message.senderId === currentUser.uid
+             ? currentUser.photoURL
+             : data.user.photoURL
+          }
         alt="" 
         />
         <span>Just now</span>
        </div>
 
        <div className="messageContent">
-        <p>Hello</p>
-        <img src='https://img.freepik.com/free-photo/young-beautiful-woman-pink-warm-sweater-natural-look-smiling-portrait-isolated-long-hair_285396-896.jpg' 
-        alt='' 
-        />
+        <p>{message.text}</p>
+        {message.img && <img src={message.img} alt='' />}
        </div>
     </div>
   )
